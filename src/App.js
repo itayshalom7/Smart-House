8e2b564d8428570e099a143210021ec58d51f602
Itay shalom <itayshalom70@gmail.com>
2022-08-14 19:43:16 +0300

    my project

---
+++ src/App.js
@@ -0,0 +1,72 @@
+
+import React, { useState } from 'react';
+import './App.css';
+import HomePage from './component/HomePage';
+import AddRoomPage from './component/AddRoomPage';
+import Header from './component/Header';
+import AddDecoPage from './component/AddDecoPage';
+import EditRoom from './component/EditRoom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MyRooms from './component/MyRooms';
+
+
+function App() {
+  const [rooms, setRooms] = useState([])
+
+  let addRoom = (room) => {
+    setRooms([...rooms, room])
+  }
+
+  let addNewDeco = (name, newDeco) => {
+    console.log("test");
+    let r = rooms.map((i) => {
+      if (i.name == name) {
+        return ({ name: i.name, roomType: i.roomType, color: i.color, items: [...i.items, newDeco] })
+      }
+      else {
+        return ({ name: i.name, roomType: i.roomType, color: i.color, items: i.items })
+      }
+    })
+    setRooms(r)
+  }
+
+  let checkIfRoomInUse = (currentRoomName) => {
+    let check=false
+    rooms.map((i) => {
+      if (currentRoomName == i.name)
+        check=true
+    })
+    return check
+  }
+  let turnOnOrOff = (name, index, status) => {
+    setRooms(rooms.map((i) => {
+      if (i.name == name) {
+        let l = i.items.map((i, n) => {
+          if (n == index) {
+            return { name: i.name, status: status }
+          }
+          else return i
+        })
+        console.log(i)
+        return ({ name: i.name, roomType: i.roomType, color: i.color, items: l })
+      }
+      else {
+        return ({ name: i.name, roomType: i.roomType, color: i.color, items: i.items })
+      }
+    }))
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <Router>
+        <Routes>
+          <Route path={'/'} element={<HomePage />} />
+          <Route path={'/addroom'} element={<AddRoomPage addRoom={addRoom} checkIfRoomInUse={checkIfRoomInUse}/>} />
+          <Route path={'/myrooms'} element={<MyRooms rooms={rooms} />} />
+          {rooms.map((item) => { return (<Route path={"room/" + item.name} element={<EditRoom room={item} addNewDeco={addNewDeco} turnOnOrOff={turnOnOrOff} />} />); })}
+        </Routes>
+      </Router>
+    </div>)
+}
+export default App;
